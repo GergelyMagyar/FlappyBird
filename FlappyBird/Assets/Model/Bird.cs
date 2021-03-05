ffc3eefde046ad37458a888a7098b17a5d439f02
GergelyMagyar <m.gergely1999@gmail.com>
2021-03-05 21:37:44 +0100

    bird movement

--- FlappyBird/Assets/Model/Bird.cs
+++ FlappyBird/Assets/Model/Bird.cs
@@ -16,19 +16,38 @@ public class Bird
 
     private bool _inJump;
 
-    public Bird(float size = 2.5f, float jumpTimeSec = 0.5f)
+    private float _speed;
+
+    private float _jumpSpeed;
+
+    public Bird(float size = 2.5f, float jumpTimeSec = 0.5f, float speed = 0.1f, float jumpSpeed = 0.1f)
     {
-        _position = new Vector2(0, 0);
+        _position = new Vector2(0f, 0f);
         _size = size;
         _dead = false;
         _jumpTimeSec = jumpTimeSec;
         _countDown = 0f;
         _inJump = false;
+        _speed = speed;
+        _jumpSpeed = jumpSpeed;
     }
 
     public void Update()
     {
+        //cooldown
 
+        Vector2 movement;
+
+        if(_inJump)
+        {
+            movement = new Vector2(_speed, _jumpSpeed);
+        }
+        else
+        {
+            movement = new Vector2(_speed, -_jumpSpeed);
+        }
+
+        _position += movement;
     }
 
     public void Jump()
--- FlappyBird/Assets/Model/GameModel.cs
+++ FlappyBird/Assets/Model/GameModel.cs
@@ -7,15 +7,25 @@ public class GameModel
 {
     private int _columnSize;
     private List<Column> _columns;
+    private Bird _bird;
+
     public GameModel(int columnSize, int rowSize, int cellSize = 10)
     {
         _columnSize = columnSize;
         _columns = new List<Column>();
+        _bird = new Bird();
         
         for(int i = 0; i < rowSize; i++)
         {
             _columns.Add(new Column(_columnSize));
         }
+    }
+
+    public void Update()
+    {
+        _bird.Update();
+
+        //check for collission
     }
 
     public void Forward()
